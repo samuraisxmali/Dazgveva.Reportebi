2c4b2384f778bfd7b42d90e4fb2f497d0f09bbb9
Archil Bolkvadze <a.bolkvadze@gmail.com>
2013-03-01 11:33:11 +0400

    201303

--- Dazgveva.Reportebi/Models/Class1.cs
+++ Dazgveva.Reportebi/Models/Class1.cs
@@ -15,7 +15,7 @@ namespace Dazgveva.Reportebi.Models
         DateTime? BIRTH_DATE { get; }
     }
 
-    public partial class DAZGVEVA_201302 : IChevicavPirovnebisRekvizitebs
+    public partial class DAZGVEVA_201303 : IChevicavPirovnebisRekvizitebs
     {
     }
 
